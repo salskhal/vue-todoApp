97cb96120afd9118563d7886a48e1e065cc87648
salskhal <olaniyisal@gmail.com>
2022-09-04 08:57:08 +0100

    feat / local storage name

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,43 @@
 <template>
-  <h1>#todo</h1>
+  <div class="al">
+    <h1>Hi There <input type="text" placeholder="your name" v-model="name"></h1>
+  </div>
 </template>
 
+<script setup>
+const { ref }=require("@vue/reactivity");
+const { watch, onMounted }=require("@vue/runtime-core");
+
+  let name = ref("")
+  watch(name, (newVal) => {
+  localStorage.setItem("name", newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || ""
+})
+</script>
+
 <style scoped>
+
+  .al{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 h1 {
   display: flex;
   justify-content: center;
   margin-top: 30px;
   color: #333333;
+  font-size: 20px;
+}
+
+input{
+  border: none;
+  margin-left: 10px;
+  outline: none;
+  font-size: 20px;
+  font-weight: bold;
 }
 </style>
